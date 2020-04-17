9125bbbf76167dc179f6d2ddab61f9f28a9f1d88
x-7r3em <vasilegov@gmail.com>
2020-04-17 14:32:11 +0300

    Init repo

---
+++ Chat/src/main/java/demo/chat/GreetingController.java
@@ -0,0 +1,19 @@
+package demo.chat;
+
+import demo.chat.models.Greeting;
+import demo.chat.models.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class GreetingController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(Message message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+
+    }
+}
