d232c62c426bd731080e055ddc759331c1863d9a
X-7r3em <vasilegov@gmail.com>
2020-05-28 17:47:26 +0300

    Removed unused packages

--- MicroserviceApp/EmployeeConsumer/src/main/java/app/FilterController.java
+++ MicroserviceApp/EmployeeConsumer/src/main/java/app/FilterController.java
@@ -1,12 +1,10 @@
 package app;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Collection;
 
 @RestController
 public class FilterController {
