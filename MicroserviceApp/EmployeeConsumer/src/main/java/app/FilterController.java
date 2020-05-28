6fdb1a38663b060b1e84b181cf7c507aec074049
X-7r3em <vasilegov@gmail.com>
2020-05-28 17:45:41 +0300

    update zuul filter

--- MicroserviceApp/EmployeeConsumer/src/main/java/app/FilterController.java
+++ MicroserviceApp/EmployeeConsumer/src/main/java/app/FilterController.java
@@ -1,19 +1,21 @@
 package app;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collection;
 
 @RestController
 public class FilterController {
 
     @GetMapping("/")
-    public String getFilter(HttpServletRequest request) {
+    public String getFilter(HttpServletRequest request, HttpServletResponse response) {
 
         Object valueObject = request.getHeader("pre");
         String value = valueObject.toString();
-        System.out.println(value);
 
         return value;
     }
