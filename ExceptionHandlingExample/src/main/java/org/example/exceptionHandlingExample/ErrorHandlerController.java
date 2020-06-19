6d51804303543dc64bfac154a173491c94221311
X-7r3em <vasilegov@gmail.com>
2020-06-19 12:55:25 +0300

    Spring Exception Handling Example

---
+++ ExceptionHandlingExample/src/main/java/org/example/exceptionHandlingExample/ErrorHandlerController.java
@@ -0,0 +1,39 @@
+package org.example.exceptionHandlingExample;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class ErrorHandlerController {
+    /**
+     * Global exception handler for all the Controllers.
+     *
+     * {@link ExceptionHandler} shows us which Exceptions are handled by the method.
+     * The method argument must match the caught exception, or be its parent, or
+     * the Exception Handler will not catch it and throw the default exception.
+     *
+     * @param ex type of exceptions to be caught
+     * @return - response entity
+     * @throws Exception
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(RuntimeException ex) throws Exception {
+        /**
+         * If I have any exceptions that are annotated with {@link ResponseStatus},
+         * this will throw them to the client as they are locally, instead of overriding them
+         * with the Global Handler.
+         *
+         * If I do not re-throw it, it will be overriden by the Global Handler.
+         */
+        if (ex.getClass().isAnnotationPresent(ResponseStatus.class)) {
+            throw ex;
+        }
+
+        ResponseEntity<String> response = new ResponseEntity<>("This is GLOBAL EXCEPTION", HttpStatus.I_AM_A_TEAPOT);
+
+        return response;
+    }
+}
---
+++ ExceptionHandlingExample/src/main/java/org/example/exceptionHandlingExample/HomeController.java
@@ -0,0 +1,25 @@
+package org.example.exceptionHandlingExample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String getHome() {
+        return "Hello";
+    }
+
+    @GetMapping("/cus")
+    public String getCustomException() {
+        throw new CustomException("This is my Custom Exception.");
+    }
+
+
+    @GetMapping("/ex")
+    public String getException() throws Exception {
+        throw new Exception("This is Local Checked Exception.");
+    }
+
+}
