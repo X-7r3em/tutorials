e92533bd1bd50c1e654731787917655c4229f100
X-7r3em <vasilegov@gmail.com>
2020-08-18 17:32:58 +0300

    Added Swagger Example

--- SwaggerExample/src/main/java/com/example/swaggerexample/SwaggerexampleApplication.java
+++ SwaggerExample/src/main/java/com/example/swaggerexample/SwaggerexampleApplication.java
@@ -2,12 +2,52 @@ package com.example.swaggerexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Collections;
+
+/**
+ * Enable swagger 2 can be put on a Configuration class
+ */
 @SpringBootApplication
+@EnableSwagger2
 public class SwaggerexampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SwaggerexampleApplication.class, args);
+	}
+
+	/**
+	 * Configuration class for the Swagger UI look on /swagger-ui.html
+	 * @return Docket
+	 */
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				// Displays specification only for the selected routes
+				.paths(PathSelectors.ant("/api/*"))
+				.build()
+				.apiInfo(configureApiInfo());
+	}
+
+	private ApiInfo configureApiInfo() {
+		return new ApiInfo(
+				"This is the title",
+				"This is the description",
+				"The best version",
+				"Terms of service URL ofcourse",
+				new Contact("Contact name", "Contact URL", "Contact email. Please dont."),
+				"I gues I have a license",
+				"This is the license URL",
+				Collections.emptyList()
+		);
 	}
 
 }
---
+++ SwaggerExample/src/main/java/com/example/swaggerexample/endpoints/ContactsController.java
@@ -0,0 +1,38 @@
+package com.example.swaggerexample.endpoints;
+
+import com.example.swaggerexample.models.Contact;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/api")
+public class ContactsController {
+
+    Map<String, Contact> contactsById = new HashMap<>();
+
+    @GetMapping("/")
+    @ApiOperation(value = "This endpoint returns all the contacts", notes = "Bla bla notes", response = Contact.class)
+    public List<Contact> getAllContacts() {
+        return new ArrayList<>(contactsById.values());
+    }
+
+    // ApiOperation is swagger documentation for the Endpoint
+    // ApiParam is swagger documentation for the Parameters of the Endpoint
+    @GetMapping("/{id}")
+    @ApiOperation(value = "This endpoint returns the contact with id",
+            notes = "Notes about this endpoint",
+            response = Contact.class)
+    public Contact getContact(@ApiParam(value = "This is the Id of the contact", required = true)
+            @PathVariable String id) {
+        return contactsById.get(id);
+    }
+
+    @PostMapping("/")
+    public Contact addContact(@RequestBody Contact contact) {
+        contactsById.put(contact.getId(), contact);
+        return contactsById.get(contact.getId());
+    }
+}
