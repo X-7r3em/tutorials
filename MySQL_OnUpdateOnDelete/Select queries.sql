bbfeff700d595f62bfc172259fba4ea95ad835cf
X-7r3em <vasilegov@gmail.com>
2020-09-01 10:46:44 +0300

    Added On Update And On Delete Examples

---
+++ MySQL_OnUpdateOnDelete/Select queries.sql
@@ -0,0 +1,9 @@
+SELECT * FROM demodb_db.warehouses;
+
+SELECT * FROM demodb_db.books;
+
+SELECT * FROM demodb_db.students;
+
+SELECT * FROM demodb_db.courses;
+
+SELECT * FROM demodb_db.students_courses;
---
+++ MySQL_OnUpdateOnDelete/Update and Delete queries.sql
@@ -0,0 +1,10 @@
+UPDATE warehouses 
+SET id = 10
+WHERE id = 1;
+
+UPDATE warehouses 
+SET id = 10
+WHERE id = 1;
+
+DELETE FROM courses 
+WHERE id = 1;
