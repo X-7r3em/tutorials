dbf4ff5710d162bf3b97a0591054764e22cd4f57
X-7r3em <vasilegov@gmail.com>
2020-11-11 12:57:26 +0200

    Added Redis Example

---
+++ Redis/src/test/java/com/example/redis/RedisTest.java
@@ -0,0 +1,21 @@
+package com.example.redis;
+
+import org.junit.jupiter.api.Test;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.params.SetParams;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RedisTest {
+
+    @Test
+    public void savingStringWithJedis() {
+        // Do not forget to start the Redis server!
+        Jedis jedis = new Jedis();
+        SetParams setParams = new SetParams();
+        setParams.ex(10);
+        jedis.set("Key", "Value", setParams);
+        assertEquals("Value", jedis.get("Key"));
+    }
+
+}
