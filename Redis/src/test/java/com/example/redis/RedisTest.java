57a9c4241c5f8be5064a7f0a6c7865874fff06fe
X-7r3em <vasilegov@gmail.com>
2020-11-21 21:24:05 +0200

    Redis delete example

--- Redis/src/test/java/com/example/redis/RedisTest.java
+++ Redis/src/test/java/com/example/redis/RedisTest.java
@@ -5,6 +5,7 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.params.SetParams;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class RedisTest {
 
@@ -19,12 +20,22 @@ public class RedisTest {
     }
 
     @Test
+    public void deletingStringWithJedis() {
+        // Do not forget to start the Redis server!
+        Jedis jedis = new Jedis();
+        jedis.set("Key", "Value");
+        assertEquals("Value", jedis.get("Key"));
+        jedis.del("Key");
+        assertNull(jedis.get("Key"));
+    }
+
+    @Test
     public void savingHashesWithJedis() {
         // Do not forget to start the Redis server!
         Jedis jedis = new Jedis();
         jedis.hset("key", "field", "value");
         jedis.expire("key", 20);
-        assertEquals("value", jedis.hget("key","field"));
+        assertEquals("value", jedis.hget("key", "field"));
     }
 
 }
