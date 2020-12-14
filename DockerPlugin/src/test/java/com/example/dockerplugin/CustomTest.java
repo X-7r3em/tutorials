3fbed15c3c7a162466159eced2743e8875b61f68
X-7r3em <vasilegov@gmail.com>
2020-12-14 19:46:01 +0200

    Docker Plugin Example

---
+++ DockerPlugin/src/test/java/com/example/dockerplugin/CustomTest.java
@@ -0,0 +1,15 @@
+package com.example.dockerplugin;
+
+import org.junit.jupiter.api.Test;
+
+public class CustomTest {
+    @Test
+    public void test() {
+    
+    }
+
+    @Test
+    public void badTest() {
+
+    }
+}
