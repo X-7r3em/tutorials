9e2e1cffbff115c00e6dac3029378d04d51242c4
X-7r3em <45735901+X-7r3em@users.noreply.github.com>
2021-03-11 18:47:24 +0200

    Added dotNet Examples

---
+++ dotNet/TechModuleExample/CaesarCipher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string decryptedMessage = Console.ReadLine();
+
+            StringBuilder encryptedMessage = new StringBuilder();
+
+            for (int i = 0; i < decryptedMessage.Length; i++)
+            {
+                encryptedMessage.Append((char)(decryptedMessage[i] + 3));
+            }
+
+            Console.WriteLine(encryptedMessage);
+        }
+    }
+}
---
+++ dotNet/TechModuleExample/Orders/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double> pricesByProduct = new Dictionary<string, double>();
+            Dictionary<string, int> amountByProduct = new Dictionary<string, int>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if (command == "buy")
+                {
+                    break;
+                }
+
+                string[] tokens = command.Split(" ");
+                string product = tokens[0];
+                double price = double.Parse(tokens[1]);
+                int amount = int.Parse(tokens[2]);
+
+                pricesByProduct[product] = price;
+
+                if (!amountByProduct.ContainsKey(product))
+                {
+                    amountByProduct[product] = 0;
+                }
+
+                amountByProduct[product] += amount;
+            }
+
+            foreach (var product in amountByProduct)
+            {
+                double average = product.Value * pricesByProduct[product.Key];
+                Console.WriteLine($"{product.Key} -> {average:F2}");
+            }
+
+            var birthday = new { a = 1, b = 2, c = 3 };
+        }
+    }
+}
